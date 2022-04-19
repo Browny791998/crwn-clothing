6b902d36ce70e66f145f4cf578051bd8dc5de6f1
Browny791998 <69458990+Browny791998@users.noreply.github.com>
2022-04-19 18:52:52 +0630

    broken our image into smaller components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './homepage.style.min.css';
+import Directory from "../../components/directory/directory.components";
+const HomePage = () =>(
+  <div className="homepage">
+    <Directory></Directory>
+  </div>
+)
+
+export default HomePage;
